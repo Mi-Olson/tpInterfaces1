a6582583ed94867378a54371bca00aa2ff28d6cd
Miriamolson73 <54155622+Miriamolson73@users.noreply.github.com>
2021-05-13 22:45:44 -0300

    2entrega

---
+++ codigo.js
@@ -0,0 +1,14 @@
+(function(){
+    var  controlPassword = document.getElementById("password");
+    var valor=controlPassword.value;
+    
+
+    if (valor < 5) {
+     return alert("la clave no es la correcta");   
+    }
+
+    
+     
+
+    }()); 
+   
