5cdf01366ae12d0ed0228602eb013fe5d5de7f16
Miriamolson73 <54155622+Miriamolson73@users.noreply.github.com>
2021-05-14 09:17:50 -0300

    2entrega14-05

--- codigo.js
+++ codigo.js
@@ -1,14 +1,52 @@
 (function(){
-    var  controlPassword = document.getElementById("password");
-    var valor=controlPassword.value;
-    
 
-    /*if (valor < 5) {
-     return alert("la clave no es la correcta");   
-    }*/
-
-    
+    document.getElementById("botonControlUsuario").addEventListener("click",function(){
+        var palabra=document.getElementById("ingresoUsuario");    
+        
+        var pal=palabra.typeOff;
+       
+        if (pal == text){alert("error debe ingresar solo numeros");}
      
+     
+     });   
 
-    }()); 
+ 
+
+document.getElementById("botonControlPassword").addEventListener("click",function(){
+   var palabra=document.getElementById("ingresoPassword");    
    
+   var pal=palabra.value;
+   var cant=pal.length;
+   if (cant<5){alert("error debe ingresar mas e 4 digitos");}
+
+
+});
+
+  
+/*id="botonControlUsuario"id="botonControlPassword"*/
+
+
+   
+
+
+}());/*
+(function(){
+    var body=document.querySelector("#body");
+    
+    
+    
+    document.getElementById('button').addEventListener("click", function(){
+     
+     var parrafoNuevo= document.createElement("p");
+     var contenidoParrafo=document.createTextNode("BEEP");
+     parrafoNuevo.appendChild(contenidoParrafo);
+    
+     //body.insertAdjacentHTML("beforeend",parrafobutton);
+     body.appendChild(parrafoNuevo);
+        
+     body.classList.toggle("color");
+     
+     
+     }	);*/
+     
+    
